2e5f19e7606211638dbce15f3eb59d0b6782be90
Ajay Shanker <ajayshanker1998@gmail.com>
2026-01-30 13:56:55 +0800

    Update Support for Radxa GPU

--- record_telemetry.sh
+++ record_telemetry.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 FLIGHT_ID=$(date +"%Y-%m-%d_%H-%M-%S")
-FLIGHT_DIR="/logs/flight_telemetry_${FLIGHT_ID}"
+FLIGHT_DIR="$HOME/logs/flight_telemetry_${FLIGHT_ID}"
 
 mkdir -p "$FLIGHT_DIR"
 # Create syslink
 ln -sfn "$FLIGHT_DIR" /logs/flight_telemetry
 
-sudo telegraf --config /etc/telegraf/telegraf.conf
+echo "Flight folder: $FLIGHT_DIR"
+echo "Symlink ~/logs/flight_telemetry -> $FLIGHT_DIR"
+
+sudo telegraf --config ./telegraf.conf
+
